f4672b011c5cab23fe16c3daf016a804ca62005f
mayankjoshi <mayankmonu9182j@gmail.com>
2025-04-23 20:00:35 +0530

    feat: add Clerk auth and schema

---
+++ app/(auth)/layout.js
@@ -0,0 +1,5 @@
+const authLayout=({children})=>{
+    return <div className="flex justify-center pt-40">{children}</div>
+}
+
+export default authLayout;
---
+++ app/dashboard/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-white w-full h-full flex justify-center items-center'>page</div>
+  )
+}
+
+export default page
